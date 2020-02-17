dcac8341831ddc93a724c90c4d2babbe7c2bebb0
Tonio993 <tonio.iuele@gmail.com>
2020-02-17 23:13:51 +0100

    Ripristino progetto

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD78Y_BM506_xzWfkIZt4_kRJNOlmtawDQ",
+    authDomain: "app-junk.firebaseapp.com",
+    databaseURL: "https://app-junk.firebaseio.com",
+    projectId: "app-junk",
+    storageBucket: "app-junk.appspot.com",
+    messagingSenderId: "632113155223"
+  }
+};
